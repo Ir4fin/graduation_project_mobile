84fe375ccd9b9992901aeecae44f52d040231298
Ir4fin <messu91@gmail.com>
2022-11-25 16:40:25 +0500

    initial commit

--- src/test/java/tests/WikiTest.java
+++ src/test/java/tests/WikiTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -10,6 +11,7 @@ import static com.codeborne.selenide.Selenide.$;
 public class WikiTest extends TestBase {
 
     @Test
+    @Tag("mobile")
     void openScreenTest(String content) {
 
         $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView")).shouldHave(text(content));
